ea18c89e15ff6124399e87d97c8acfa3c92a70f1
htbkoo <h1017760@connect.hku.hk>
2018-04-26 08:05:16 +0800

    (WIP) added test to drive the development for the reverse() from React inline style back to CSS htbkoo/CssToAndFromReact

--- src/test/transform.test.js
+++ src/test/transform.test.js
@@ -1,29 +1,21 @@
-var css = require("css");
-// var reactCSS = require('reactcss');
-var reactCSS = require('reactcss').default;
+import chai from 'chai';
+
+import {reverse} from '../transform';
 
 describe("transform", function () {
-    it("spec name", function () {
-        var reactText = JSON.stringify({"a": {"color": "red"}});
+    describe("reverse", function () {
+        it("should reverse reactStyleObjText to css text", function () {
+            // given
+            const reactText = JSON.stringify({"body": {"marginLeft": "5%"}});
 
-        // let parsed = JSON.parse(reactText);
-        var result = reactCSS({'default': JSON.parse(reactText)});
+            // when
+            let resultCssText = reverse(reactText);
 
-        console.log(JSON.stringify(result));
-    });
-
-    xit("spec nam e", function () {
-        // var ast = css.parse('body { font-size: 12px; }', { source: 'source.css' });
-        var ast = css.parse('body { font-size: 12px; }');
-
-        // var css = css.stringify(ast);
-
-        var result = css.stringify(ast, {sourcemap: true});
-    });
-
-    xit("should try css parser", function () {
-        var obj = css.parse("body {margin-right: #FFFFF;}");
-        console.log(css.stringify("{\"body\":{\"marginRight\":\"#FFFFF\"}}"));
+            // then
+            const expectedCssText = "body {\n" +
+                " margin-left: 5%;\n" +
+                "}";
+            chai.expect(resultCssText).to.equal(expectedCssText);
+        });
     });
 });
-
