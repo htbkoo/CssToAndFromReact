d554da17361e835cf684ff417e5ed068a4994b94
htbkoo <h1017760@connect.hku.hk>
2018-05-02 08:28:45 +0800

    refactored reverse.ts htbkoo/CssToAndFromReact

--- src/reverse.ts
+++ src/reverse.ts
@@ -4,8 +4,12 @@ import postcssJs from "postcss-js";
 import autoprefixer from "autoprefixer";
 
 export function promiseReverse(inputReactObjText) {
-    return new Promise(resolve => resolve(JSON.parse(inputReactObjText)))
+    return promiseParseJson(inputReactObjText)
         .then(obj => postcss([autoprefixer])
             .process(obj, {parser: postcssJs, from: undefined})
         );
+}
+
+function promiseParseJson(inputReactObjText) {
+    return new Promise(resolve => resolve(JSON.parse(inputReactObjText)));
 }
