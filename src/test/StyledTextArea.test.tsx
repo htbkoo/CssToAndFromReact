24caa5251f4c43bc8ad3572cb2e5785d7a106f37
htbkoo <h1017760@connect.hku.hk>
2018-05-15 19:42:43 +0800

    updated StyledTextArea.test.tsx htbkoo/CssToAndFromReact

--- src/test/StyledTextArea.test.tsx
+++ src/test/StyledTextArea.test.tsx
@@ -6,13 +6,18 @@ import StyledTextArea from "../StyledTextArea";
 import {NO_OP} from "../utils/utils";
 
 describe("StyledTextArea", function () {
-    it("should shallow without error", function () {
-        shallow(
+    it("should shallow without error and contain a <textarea>", function () {
+        // given
+        // when
+        let wrapper = shallow(
             <StyledTextArea
                 value="someValue"
                 placeholder="somePlaceholder"
                 onChange={NO_OP}
             />
         );
+
+        // then
+        expect(wrapper.find("textarea").length).toEqual(1);
     });
 });
