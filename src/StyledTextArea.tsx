b1c16261a2853ab9227ccf0e4140ee5893d73901
htbkoo <h1017760@connect.hku.hk>
2018-05-16 08:29:09 +0800

    added defaultProps.isError in StyledTextArea.tsx htbkoo/CssToAndFromReact

--- src/StyledTextArea.tsx
+++ src/StyledTextArea.tsx
@@ -5,11 +5,16 @@ type StyledTextAreaProps = {
     value: string,
     placeholder: string,
     onChange: (event) => void,
-    style?: CSSProperties
+    style?: CSSProperties,
+    isError?: boolean
 };
 type StyledTextAreaState = {};
 
 export default class StyledTextArea extends React.Component<StyledTextAreaProps, StyledTextAreaState> {
+    static defaultProps: Partial<StyledTextAreaProps> = {
+        isError: false
+    };
+
     componentDidUpdate(prevProps) {
         let node = ReactDOM.findDOMNode(this) as HTMLInputElement; // reference: https://github.com/Microsoft/TypeScript/issues/10453#issuecomment-301263769
         let oldLength = node.value.length;
