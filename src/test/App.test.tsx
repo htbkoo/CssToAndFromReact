f058f6cfbf5507edff01d901673049500666cedf
htbkoo <h1017760@connect.hku.hk>
2018-05-18 08:45:46 +0800

    cleaned up App.test.tsx htbkoo/CssToAndFromReact

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -7,31 +7,34 @@ import StyledTextArea from "../StyledTextArea";
 import * as transform from '../transform';
 
 describe("App", function () {
-    it("should have 2 <StyledTextArea/> and 1 <input/> when rendered", function () {
-        // given
-        // when
-        let wrapper = shallow(<App/>);
+    describe("construction", function () {
+        it("should have 2 <StyledTextArea/> and 1 <input/> when rendered", function () {
+            // given
+            // when
+            let wrapper = shallow(<App/>);
 
-        // then
-        expect(wrapper.find(StyledTextArea).length).toEqual(2);
-        expect(wrapper.find("input").length).toEqual(1);
+            // then
+            expect(wrapper.find(StyledTextArea).length).toEqual(2);
+            expect(wrapper.find("input").length).toEqual(1);
+        });
     });
 
-    // it("should, on input valid change, update output", sinonTest(function (this: sinon.SinonSandbox) {
-    it("should, on input valid change, update output", sinonTest(function () {
-        // given
-        const input = "someInput", result = {"key": "value"};
-        this.stub(transform, "transform").withArgs(input).returns(result);
+    describe("forward transformation", function () {
+        it("should, on input valid change, update output", sinonTest(function () {
+            // given
+            const input = "someInput", result = {"key": "value"};
+            this.stub(transform, "transform").withArgs(input).returns(result);
 
-        let wrapper = shallow(<App/>);
+            let wrapper = shallow(<App/>);
 
-        // when
-        let inputTextArea = wrapper.find(StyledTextArea).at(0);
-        inputTextArea.simulate("change", {target: {value: input}});
+            // when
+            let inputTextArea = wrapper.find(StyledTextArea).at(0);
+            inputTextArea.simulate("change", {target: {value: input}});
 
-        // then
-        expect(wrapper.state("inputText")).toEqual(input);
-        const expectedOutput = JSON.stringify(result);
-        expect(wrapper.state("outputText")).toEqual(expectedOutput);
-    }));
+            // then
+            expect(wrapper.state("inputText")).toEqual(input);
+            const expectedOutput = JSON.stringify(result);
+            expect(wrapper.state("outputText")).toEqual(expectedOutput);
+        }));
+    });
 });
