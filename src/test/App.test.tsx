319301679b38821f496156c4e6677f0ec56c0b8e
htbkoo <h1017760@connect.hku.hk>
2018-05-18 08:43:35 +0800

    created sinonWithTest.js; added text for forwardTransformation (happyPath) in App.test.tsx htbkoo/CssToAndFromReact

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,8 +1,10 @@
 import {shallow} from "enzyme";
 import * as React from 'react';
+import {sinonTest} from './utils/sinonWithTest'
 
 import App from '../App';
 import StyledTextArea from "../StyledTextArea";
+import * as transform from '../transform';
 
 describe("App", function () {
     it("should have 2 <StyledTextArea/> and 1 <input/> when rendered", function () {
@@ -14,4 +16,22 @@ describe("App", function () {
         expect(wrapper.find(StyledTextArea).length).toEqual(2);
         expect(wrapper.find("input").length).toEqual(1);
     });
+
+    // it("should, on input valid change, update output", sinonTest(function (this: sinon.SinonSandbox) {
+    it("should, on input valid change, update output", sinonTest(function () {
+        // given
+        const input = "someInput", result = {"key": "value"};
+        this.stub(transform, "transform").withArgs(input).returns(result);
+
+        let wrapper = shallow(<App/>);
+
+        // when
+        let inputTextArea = wrapper.find(StyledTextArea).at(0);
+        inputTextArea.simulate("change", {target: {value: input}});
+
+        // then
+        expect(wrapper.state("inputText")).toEqual(input);
+        const expectedOutput = JSON.stringify(result);
+        expect(wrapper.state("outputText")).toEqual(expectedOutput);
+    }));
 });
