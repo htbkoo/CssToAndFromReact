0e786108c5fb2548d577e89d4ded71b802533dd9
htbkoo <htbkoo+github@proton.me>
2024-06-09 17:24:57 +0200

    feat: Update formatting

--- src/reverse.ts
+++ src/reverse.ts
@@ -1,15 +1,17 @@
-import { default as postcss }  from "postcss";
+import { default as postcss } from "postcss";
 // noinspection TypeScriptCheckImport
 import postcssJs from "postcss-js";
 import autoprefixer from "autoprefixer";
 
 export function promiseReverse(inputReactObjText: string) {
-    return promiseParseJson(inputReactObjText)
-        .then(obj => postcss([autoprefixer])
-            .process(obj, {parser: postcssJs, from: undefined})
-        );
+  return promiseParseJson(inputReactObjText).then((obj) =>
+    postcss([autoprefixer]).process(obj, {
+      parser: postcssJs,
+      from: undefined,
+    }),
+  );
 }
 
 function promiseParseJson(inputReactObjText: string) {
-    return new Promise(resolve => resolve(JSON.parse(inputReactObjText)));
+  return new Promise((resolve) => resolve(JSON.parse(inputReactObjText)));
 }
