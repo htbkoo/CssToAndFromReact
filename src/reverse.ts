833270a419ec4c04a137926f17efaab466a4a4ce
htbkoo <htbkoo+github@proton.me>
2024-06-09 17:23:10 +0200

    feat(reverse): Improve types info

--- src/reverse.ts
+++ src/reverse.ts
@@ -3,13 +3,13 @@ import { default as postcss }  from "postcss";
 import postcssJs from "postcss-js";
 import autoprefixer from "autoprefixer";
 
-export function promiseReverse(inputReactObjText) {
+export function promiseReverse(inputReactObjText: string) {
     return promiseParseJson(inputReactObjText)
         .then(obj => postcss([autoprefixer])
             .process(obj, {parser: postcssJs, from: undefined})
         );
 }
 
-function promiseParseJson(inputReactObjText) {
+function promiseParseJson(inputReactObjText: string) {
     return new Promise(resolve => resolve(JSON.parse(inputReactObjText)));
 }
